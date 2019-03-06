73e6e64bd251419d68a4536ba87e7ef6cfe30766
Joel Clarke <joel.clarke273@gmail.com>
2019-03-06 16:44:24 +0000

    Allow CueCard to iterate through a collection of possible texts

--- src/components/CueCard.vue
+++ src/components/CueCard.vue
@@ -1,30 +1,23 @@
 <template>
   <div class="cue-card">
-    <v-flex>
-      <v-sheet flat color="green" max-width="450" height="360" class="py-3">
+    <v-flex grow>
+      <v-sheet flat color="green" width="450" height="360" class="py-3">
         <v-layout column fill-height justify-space-between>
-          <v-flex class="my-5">
-            <p class="display-2">{{ text }}</p>
-          </v-flex>
 
           <v-flex>
-            <v-layout row justify-space-between>
+            <p class="subheading">
+              {{ (textIndex + 1).toString() + "/" + (possibleTexts.length).toString() }}
+            </p>
+          </v-flex>
 
-              <v-flex>
-                <v-btn large flat icon>
-                  <v-icon>fas fa-arrow-left</v-icon>
-                </v-btn>
-              </v-flex>
-
-              <v-spacer />
-
-              <v-flex>
-                <v-btn large flat icon>
-                  <v-icon>fas fa-arrow-right</v-icon>
-                </v-btn>
-              </v-flex>
-
-            </v-layout>
+          <v-flex grow class="my-3 px-3">
+            <p class="display-2">{{ possibleTexts[textIndex] }}</p>
+          </v-flex>
+              
+          <v-flex shrink>
+            <v-btn large flat icon @click="nextText">
+              <v-icon>fas fa-arrow-right</v-icon>
+            </v-btn>
           </v-flex>
 
         </v-layout>
@@ -39,9 +32,33 @@ export default {
   name: "CueCard",
   data() {
     return {
-      text: "Sphinx of Black Quartz, Judge my Vow",
-      possibleTexts: []
+      textIndex: 0,
+      possibleTexts: [
+        "Sphinx of black quartz, judge my vow",
+        "How much wood would a woodchuck chuck if a woodchuck could chuck wood?",
+        "Small",
+        "To err is human"
+      ]
     }
   },
+  methods: {
+    shuffle(a) {
+      for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]]
+      }
+    },
+
+    nextText() {
+      this.textIndex = (this.textIndex + 1) % this.possibleTexts.length
+      
+      if (this.textIndex === 0) {
+        this.shuffle(this.possibleTexts)
+      }
+    }
+  },
+  created() {
+    this.shuffle(this.possibleTexts)
+  }
 }
 </script>
