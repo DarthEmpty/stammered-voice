8bf0d63d59f06bc4c9ce0717988d15dcbb80ab8a
Joel Clarke <joel.clarke273@gmail.com>
2019-03-06 15:40:27 +0000

    Add arrows to CueCard

--- src/components/CueCard.vue
+++ src/components/CueCard.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="cue-card">
     <v-flex>
-      <v-sheet flat
-        color="green"
-        max-width="450"
-        min-height="360"
-        class="py-5"
-      >
+      <v-sheet flat color="green" max-width="450" height="360" class="py-3">
         <v-layout column fill-height justify-space-between>
           <v-flex class="my-5">
             <p class="display-2">{{ text }}</p>
           </v-flex>
+
+          <v-flex>
+            <v-layout row justify-space-between>
+
+              <v-flex>
+                <v-btn large flat icon>
+                  <v-icon>fas fa-arrow-left</v-icon>
+                </v-btn>
+              </v-flex>
+
+              <v-spacer />
+
+              <v-flex>
+                <v-btn large flat icon>
+                  <v-icon>fas fa-arrow-right</v-icon>
+                </v-btn>
+              </v-flex>
+
+            </v-layout>
+          </v-flex>
+
         </v-layout>
       </v-sheet>
     </v-flex>
