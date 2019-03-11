ab323288dd38541edf21aa625e976fb183f5b86e
Joel Clarke <joel.clarke273@gmail.com>
2019-03-11 18:14:16 +0000

    Begin login component

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="login">
+    <v-flex>
+      <v-sheet>
+        <form>
+
+        </form>
+      </v-sheet>
+    </v-flex>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: ""
+    }
+  }  
+}
+</script>
