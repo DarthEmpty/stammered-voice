99632e5d1393db1598f48fd280c25bcd5d272fa1
Joel Clarke <joel.clarke273@gmail.com>
2019-03-12 14:58:02 +0000

    Add validation to login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,34 @@
           <v-text-field
             id="username"
             name="username"
+            v-model="username"
             label="Enter your username"
+            v-validate="'required|alpha_dash'"
+            :error-messages="errors.collect('username')"
             outline
+            class="my-2"
           />
           <v-text-field
             id="password"
             name="password"
+            v-model="password"
             label="Enter your password"
+            v-validate="'required'"
+            :error-messages="errors.collect('password')"
             :type="show ? 'text' : 'password'"
             :append-icon="show ? 'fas fa-eye' : 'fas fa-eye-slash'"
             @click:append="show = !show"
             outline
+            class="my-2"
           />
-          <v-btn large depressed color="green" @click="submit">Submit</v-btn>
+          <v-btn
+            large depressed
+            :disabled="isDisabled"
+            color="green" 
+            @click="submit"
+          >
+            Submit
+          </v-btn>
         </form>
       </v-sheet>
     </v-flex>
@@ -28,18 +43,30 @@
 
 <script>
 export default {
+  $_veeValidate: {
+    validator: "new"
+  },
+
   name: "Login",
   data() {
     return {
       username: "",
       password: "",
       show: false
+    };
+  },
+  computed: {
+    isDisabled() {
+      return this.username === "" || this.password === ""
     }
   },
   methods: {
-    submit() {
-      this.$emit("log-in");
+    async submit() {
+      let result = await this.$validator.validate()
+      if (result) {
+        this.$emit("log-in")
+      }
     }
   }
-}
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify from "vuetify"
 import 'vuetify/dist/vuetify.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import VeeValidate from 'vee-validate'
 import App from './App.vue'
 
 Vue.config.productionTip = false
@@ -9,6 +10,8 @@ Vue.config.productionTip = false
 Vue.use(Vuetify, {
   iconfont: "fa"
 })
+
+Vue.use(VeeValidate)
 
 new Vue({
   render: h => h(App),
