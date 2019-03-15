59b7f74dfc5831def60031bfdeb0f1dfaa020577
Joel Clarke <joel.clarke273@gmail.com>
2019-03-15 15:24:59 +0000

    Make Feathers Errors display in a more user friendly way

--- src/components/ErrorDialog.vue
+++ src/components/ErrorDialog.vue
@@ -4,7 +4,7 @@
       max-width="500px"
       color="red lighten-4"
       class="red--text text--darken-3 pa-5">
-      <p class="body-2">{{ error.message }}</p>
+      <p class="body-2">{{ message }}</p>
       <p class="body-1">Please refresh the page, resolve this issue and try again.</p>
     </v-sheet>
   </div>
@@ -14,5 +14,14 @@
 export default {
   name: "ErrorDialog",
   props: [ "error" ],
+  computed: {
+    message() {
+      if (this.error.type === "FeathersError") {
+        return this.error.errors.map(error => error.message).join("\n")
+      }
+
+      return this.error.message
+    }
+  }
 }
 </script>
