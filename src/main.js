401258332313bfa84da38365cd0c7649d7da4f48
Joel Clarke <joel.clarke273@gmail.com>
2019-06-14 14:32:21 +0100

    Initialise a global store

--- src/main.js
+++ src/main.js
@@ -5,16 +5,15 @@ import '@fortawesome/fontawesome-free/css/solid.min.css'
 import '@fortawesome/fontawesome-free/css/brands.min.css'
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
 import VeeValidate from 'vee-validate'
+import store from "./store"
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
-Vue.use(Vuetify, {
-  iconfont: "fa"
-})
-
+Vue.use(Vuetify, { iconfont: "fa" })
 Vue.use(VeeValidate)
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
