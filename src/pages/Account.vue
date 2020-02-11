09b2382b7bff13563ad7ae28da53dbd8302f826e
Joel Clarke <joel.clarke273@gmail.com>
2020-02-11 16:07:10 +0000

    Separate cases for logged in status on account page

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -1,26 +1,30 @@
 <template>
   <div id="account">
-    <v-sheet color="accent" class="pb-3">
-      <h1 class="headline py-5">{{
-        user
-        ? "Logged in as " + user.username
-        : "Not currently logged in..."
-        }}</h1>
-      
-      <v-btn flat color="primary">See Data</v-btn>
-
-      <v-btn flat color="primary">Delete Data</v-btn>
+    <v-sheet v-if="!user" color="accent">
+      <h1 class="headline py-5">
+        Not currently logged in...
+      </h1>
     </v-sheet>
 
-    <router-link :to="'/'">
-      <v-btn
-        depressed block
-        @click="logout"
-        color="primary"
-      >
-        Log Out
-      </v-btn>
-    </router-link>
+    <div v-else>
+      <v-sheet color="accent" class="pb-3">
+        <h1 class="headline py-5">Logged in as {{ user.username }}</h1>
+        
+        <v-btn flat color="primary">See Data</v-btn>
+
+        <v-btn flat color="primary">Delete Data</v-btn>
+      </v-sheet>
+
+      <router-link :to="'/'">
+        <v-btn
+          depressed block
+          @click="logout"
+          color="primary"
+        >
+          Log Out
+        </v-btn>
+      </router-link>
+    </div>
   </div>
 </template>
 
