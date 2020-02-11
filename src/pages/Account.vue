d39189399d57249cf38c6c9c0f5a90976260b4db
Joel Clarke <joel.clarke273@gmail.com>
2020-02-11 15:14:48 +0000

    Implement username info into Account page

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="account">
-    
+    <v-sheet color="accent" class="pa-5">
+      <h1 class="headline">{{
+        user
+        ? "Logged in as " + user.username
+        : "Not currently logged in..."
+        }}</h1>
+    </v-sheet>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  name: "Account"
+  name: "Account",
+  computed: {
+    ...mapState(["user"])
+  }
 }
 </script>
